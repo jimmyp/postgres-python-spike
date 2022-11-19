3223aeda9c24802b39b0692c854a0c632fb3255e
Jim Pelletier <mail@thejimmyp.com>
2022-11-19 21:41:03 +0000

    peewee works with postgres

--- peewee-spike.py
+++ peewee-spike.py
@@ -2,19 +2,24 @@ from peewee import *
 from datetime import date
 
 db = SqliteDatabase('test.db')
+psql_db = PostgresqlDatabase(
+    'peewee-spike', host='db', user='postgres', password='postgres')
+
 
 class Person(Model):
     name = CharField()
     birthday = DateField()
+    favourite_color = CharField()
 
     class Meta:
-        database = db
+        database = psql_db
+
 
 db.connect()
 
 db.create_tables([Person])
 
-Person.create(name='Bob', birthday=date(1960, 1, 15))
+Person.create(name='Bob', birthday=date(1960, 1, 15), favourite_color='blue')
 
 bob = Person.select().where(Person.name == 'Bob').get()
 
@@ -27,3 +32,5 @@ bob.save()
 bob2 = Person.select().where(Person.name == 'Robert').get()
 
 print(bob2.name, bob2.birthday)
+
+db.close()
